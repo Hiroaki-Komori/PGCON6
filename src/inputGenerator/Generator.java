4a6247f741c890a5a1cf25d63645e87bd6eb3434
Hiroaki Komori <yukikaze.inside@gmail.com>
2015-07-09 11:58:23 +0900

    generate input

--- src/inputGenerator/Generator.java
+++ src/inputGenerator/Generator.java
@@ -1,5 +1,50 @@
 package inputGenerator;
 
-public class Generator {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class Generator {
+	public static void main(String[] args) {
+		File source = new File("./bin/resources/input.txt");
+		BufferedWriter writer = null;
+		try {
+			writer = new BufferedWriter(new FileWriter(source));
+			source.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		int L = 50;
+		int N = 50;
+		List<Integer> seeds = new ArrayList<>();
+		for (int i = 1; i <= L; i++) {
+			seeds.add(i);
+		}
+		Collections.shuffle(seeds);
+		seeds = seeds.subList(0, N);
+		Collections.sort(seeds);
+		try{
+			writer.write(String.valueOf(L));
+			writer.newLine();
+			writer.write(String.valueOf(N));
+			writer.newLine();
+		for (int num: seeds) {
+			System.out.println(num);
+			writer.write(String.valueOf(num));
+			writer.newLine();
+		}
+		}catch(IOException e){
+			e.printStackTrace();
+		}finally{
+			try {
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
